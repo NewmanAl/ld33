0d1eb4cfb2358b6034544b4557de314c76d03280
Alex Newman <newman.alx@gmail.com>
2015-08-22 11:31:03 -0400

    Game window is 800x600 but graphics are scaled up from 640x480
    Working font
    
    Working mouse capture

---
+++ core/src/cosmitronique/ld33/Font/FontManager.java
@@ -0,0 +1,74 @@
+package cosmitronique.ld33.Font;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class FontManager {
+	
+	private final String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private final String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+	private final String symbols = "!@#$%^&*()[]{}+-=:;?/\\_.,\'\"";
+	private final String numbers = "0123456789><~` "; // > and < look like arrows, ~ is a blank box, ` is a checked box
+	private TextureRegion[][] fontText;
+	private Texture fontTex;
+	private float xOffset, yOffset;
+	private int texX, texY;
+	
+	public FontManager(){
+		fontTex = new Texture(Gdx.files.internal("Font/thickFont.png"));
+		fontText = TextureRegion.split(fontTex, 8, 8);
+		
+	}
+	
+	public void drawMultilinedMessage(String msg, SpriteBatch batch, float x, float y, float scale, Color color){
+		String[] lines = msg.split("\n");
+		for(String l : lines)
+		{
+			drawMessage(l,batch,x,y,scale,color);
+			y -= 8 * scale;
+		}
+	}
+	
+	public void drawMessage(String msg, SpriteBatch batch, float x, float y, float scale, Color color){
+		xOffset = yOffset = 0;
+		texX = texY = 0;
+		batch.setColor(color);
+		
+		for(int i = 0; i<msg.length(); i++){
+			//find position in letters
+			if(upperCase.indexOf(msg.charAt(i)) > -1){
+				texX = upperCase.indexOf(msg.charAt(i));
+				xOffset = i * 8 * scale;
+				texY = 0;
+			}
+			else if(lowerCase.indexOf(msg.charAt(i)) > -1){
+				texX = lowerCase.indexOf(msg.charAt(i));
+				xOffset = i * 8 * scale;
+				texY = 1;
+			}
+			else if(symbols.indexOf(msg.charAt(i)) > -1){
+				texX =  symbols.indexOf(msg.charAt(i));
+				xOffset = i * 8 * scale;
+				texY = 2;
+			}
+			else if(numbers.indexOf(msg.charAt(i)) > -1){
+				texX = numbers.indexOf(msg.charAt(i));
+				xOffset = i * 8 * scale;
+				texY = 3;
+			}else{
+				//assume it's a space
+				continue;
+			}
+			
+			//draw letter
+			batch.draw(fontText[texY][texX], x + xOffset, y + yOffset, 8 * scale, 8 * scale);
+		}
+		
+		batch.setColor(Color.WHITE);
+	}
+
+}
--- core/src/cosmitronique/ld33/Main.java
+++ core/src/cosmitronique/ld33/Main.java
@@ -2,26 +2,77 @@ package cosmitronique.ld33;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+import cosmitronique.ld33.Font.*;
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	FontManager font;
+	OrthographicCamera cam;
+	int mouseX;
+	int mouseY;
+	Texture pixel;
+	Texture testTex;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new FontManager();
+		cam = new OrthographicCamera(640,480 * (4/3));
+		cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
+        cam.update();
+        pixel = new Texture(Gdx.files.internal("Debug/pixel.png"));
+        testTex = new Texture(Gdx.files.internal("Sprites/easterEgg.png"));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		cam.update();
+		batch.setProjectionMatrix(cam.combined);
+		
+		mouseX = Gdx.input.getX();
+		mouseY = Gdx.input.getY();
+		mouseY = -mouseY + 600;
+		
+		mouseX = 640 * mouseX / 800;
+		mouseY = 480 * mouseY / 600;
+		
 		batch.begin();
-		batch.draw(img, 0, 0);
+		font.drawMessage("HELLO World. You are so lovely today :D LUDUM DARE 33", batch, 0, 0, 2f, Color.WHITE);
+		font.drawMultilinedMessage("X: " + mouseX + "\nY: " + mouseY, batch, 100, 100, 2f, Color.GREEN);
+		
+		mouseInputTest(batch);
+		
+		batch.draw(pixel, mouseX, mouseY);
 		batch.end();
 	}
+	
+	private void mouseInputTest(SpriteBatch batch){
+		Rectangle text = new Rectangle();
+		text.width = 6 * 8 * 2;
+		text.height = 2 * 8 * 2;
+		text.x= 100;
+		text.y = 100 - (text.height/2);
+		
+		Rectangle mouse = new Rectangle();
+		mouse.height = 1;
+		mouse.width = 1;
+		mouse.x = mouseX;
+		mouse.y = mouseY;
+		
+		if (mouse.overlaps(text)){
+			if(Gdx.input.isButtonPressed(0))
+				batch.draw(testTex,100,100 - (text.height/2));
+		}
+		
+	}
+	
 }
