f4ab56d521775c571bd04a4b26e52f74d26339d1
Alex Newman <newman.alx@gmail.com>
2015-08-22 08:32:56 -0400

    Initial commit for Ludum Dare 33. Default project generated by libgdx

---
+++ html/src/cosmitronique/ld33/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package cosmitronique.ld33.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import cosmitronique.ld33.Main;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Main();
+        }
+}
